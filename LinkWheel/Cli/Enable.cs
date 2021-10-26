ee1da7594f15653b0b575e6b873006b9e71227ef
Mark De Ruyter <mderu001@ucr.edu>
2021-10-25 22:36:13 -0700

    Moves enable/disable to commands
    Also adds RegisterRepo to the parsed arguments.

---
+++ LinkWheel/Cli/Enable.cs
@@ -0,0 +1,32 @@
+﻿using CommandLine;
+using Microsoft.Win32;
+using System;
+
+namespace LinkWheel.Cli
+{
+    [Verb("enable")]
+    public class Enable
+    {
+        public int Execute()
+        {
+            if (OperatingSystem.IsWindows())
+            {
+                Registry.CurrentUser.OpenSubKey(LinkWheelConfig.Registry.ClassKey, true)
+                    .SetValue(LinkWheelConfig.Registry.EnabledValue, "true");
+                return 0;
+            }
+            throw new NotImplementedException($"{nameof(Enable)} has only been implemented for Windows.");
+        }
+
+        public static bool IsEnabled()
+        {
+            if (OperatingSystem.IsWindows())
+            {
+                using RegistryKey classesRoot = Registry.ClassesRoot.OpenSubKey(nameof(LinkWheel));
+                string rawValue = (string)classesRoot.GetValue(LinkWheelConfig.Registry.EnabledValue, "false");
+                return bool.Parse(rawValue);
+            }
+            throw new NotImplementedException($"{nameof(Enable)} has only been implemented for Windows.");
+        }
+    }
+}
