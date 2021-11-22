c8f7294a87be25510bfad013b57a1fda19f0fd8a
Mark De Ruyter <mderu001@ucr.edu>
2021-11-21 23:29:28 -0800

    Support linking directly to lines in a formal way

--- CoreAPI/Cli/GetUrl.cs
+++ CoreAPI/Cli/GetUrl.cs
@@ -15,11 +15,17 @@ namespace CoreAPI.Cli
         [Option("file", Required = true)]
         public string File { get; set; }
 
+        [Option("start-line")]
+        public int? StartLine { get; set; }
+
+        [Option("end-line")]
+        public int? EndLine { get; set; }
+
         public async Task<int> ExecuteAsync()
         {
             List<RepoConfig> repoConfigs = RepoConfigFile.Read();
 
-            if (TaskUtils.Try(await RemoteRepoHosts.TryGetRemoteLinkFromPath(File, repoConfigs), out Uri remoteLink))
+            if (TaskUtils.Try(await RemoteRepoHosts.TryGetRemoteLinkFromPath(this, repoConfigs), out Uri remoteLink))
             {
                 Trace.WriteLine(remoteLink.ToString());
                 return 0;
