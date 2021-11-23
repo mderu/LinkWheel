c5880a6de16a0020a59ee291d8385fd2d00554d6
Mark De Ruyter <mderu001@ucr.edu>
2021-11-22 22:16:44 -0800

    Removes Hello World code

--- Extensions/VSCode/codelink/out/extension.js
+++ Extensions/VSCode/codelink/out/extension.js
@@ -8,18 +8,6 @@ const cp = require("child_process");
 // this method is called when your extension is activated
 // your extension is activated the very first time the command is executed
 function activate(context) {
-    // Use the console to output diagnostic information (console.log) and errors (console.error)
-    // This line of code will only be executed once when your extension is activated
-    console.log('Congratulations, your extension "codelink" is now active!');
-    // The command has been defined in the package.json file
-    // Now provide the implementation of the command with registerCommand
-    // The commandId parameter must match the command field in package.json
-    let disposable = vscode.commands.registerCommand('codelink.helloWorld', () => {
-        // The code you place here will be executed every time your command is executed
-        // Display a message box to the user
-        vscode.window.showInformationMessage('Hello World from CodeLink!');
-    });
-    context.subscriptions.push(disposable);
     // The command has been defined in the package.json file
     // Now provide the implementation of the command with registerCommand
     // The commandId parameter must match the command field in package.json
