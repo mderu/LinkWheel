aac57e11d261eb098d4acd790364ee52552194eb
Mark De Ruyter <mderu001@ucr.edu>
2021-11-24 14:04:57 -0800

    Adds multiline linking to the VSCode extension

--- Extensions/VSCode/codelink/src/extension.ts
+++ Extensions/VSCode/codelink/src/extension.ts
@@ -13,21 +13,29 @@ export function activate(context: vscode.ExtensionContext) {
 	let rightClickDisposable = vscode.commands.registerCommand('codelink.linkLine', () => {
 		// The code you place here will be executed every time your command is executed
 		// Display a message box to the user
-		let currentLine = vscode.window.activeTextEditor?.selection.active.line;
+		let startLine = vscode.window.activeTextEditor?.selection.start.line;
+		let endLine = vscode.window.activeTextEditor?.selection.end.line;
 
-		if (currentLine != null && vscode.window.activeTextEditor != null) {
+		if (startLine != null && vscode.window.activeTextEditor != null) {
+			startLine += 1;
+			endLine = endLine ? endLine + 1 : endLine;
 			cp.exec(`linkWheelCli register --path ${vscode.window.activeTextEditor?.document.fileName}`,
 			(err: any, stdout: string, stderr: string) => {
 				if (err) {
 					vscode.window.showErrorMessage(`Unable to link to the given line: ${err}: ${stderr}`)
 				}
 				else {
-					cp.exec(`linkWheelCli get-url --file ${vscode.window.activeTextEditor?.document.fileName} --start-line ${currentLine}`,
+					cp.exec(
+						`linkWheelCli get-url `
+						 + `--file ${vscode.window.activeTextEditor?.document.fileName} `
+						 + `--start-line ${startLine}`
+						 + (endLine == startLine ? "": ` --end-line ${endLine}`),
 						(err: any, stdout: string, stderr: string) => {
 							if (err) {
 								vscode.window.showErrorMessage(`Unable to link to the given line: ${err}: ${stderr}`)
 							}
 							else {
+								console.info(stdout.trim())
 								vscode.env.clipboard.writeText(stdout.trim())
 							}
 						});
