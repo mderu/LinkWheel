89beb76b5b6d98f969b7a9440b478e34e1880335
s01d <markde58@gmail.com>
2026-04-19 01:42:58 -0700

    Adds GetRegistrations
    Emits all of the registered repos, making it easier to see what you have configured.

---
+++ CoreAPI/Cli/GetRegistrations.cs
@@ -0,0 +1,23 @@
+﻿using CommandLine;
+using CoreAPI.Config;
+using CoreAPI.OutputFormat;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace CoreAPI.Cli
+{
+    [Verb("get-registrations", HelpText = HelpText)]
+    public class GetRegistrations
+    {
+        public const string HelpText = "Returns all registered repo configurations.";
+
+        public Task<OutputData> ExecuteAsync()
+        {
+            var configs = RepoConfigs.All();
+            return Task.FromResult(new OutputData(0, new Dictionary<string, object>
+            {
+                { "registrations", configs }
+            }));
+        }
+    }
+}
